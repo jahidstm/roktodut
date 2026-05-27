18647d3c4995dbfe67439904222e8713474322f1
Jahid Hasan <jahidhasanstm@gmail.com>
2026-05-27 14:29:50 +0600

    refactor: update sidebar navigation to link to my requests instead of all requests

--- resources/views/layouts/donor-dashboard.blade.php
+++ resources/views/layouts/donor-dashboard.blade.php
@@ -289,11 +289,7 @@
         {{-- রক্তদান --}}
 
 
-        <a href="{{ route('requests.index') }}"
-           class="sidebar-nav-item {{ request()->routeIs('requests.index') ? 'active' : '' }}">
-            <svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"/></svg>
-            রক্তের রিকোয়েস্ট
-        </a>
+
 
         <a href="{{ route('requests.my-requests') }}"
            class="sidebar-nav-item {{ request()->routeIs('requests.my-requests') ? 'active' : '' }}" data-tab="my-requests">
