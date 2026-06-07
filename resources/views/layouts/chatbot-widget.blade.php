12327b763a8e4bdedbe97707e537dbb57a3df530
Jahid Hasan <jahidhasanstm@gmail.com>
2026-06-07 21:59:07 +0600

    feat: implement chatbot widget UI and integration logic using Alpine.js

--- resources/views/layouts/chatbot-widget.blade.php
+++ resources/views/layouts/chatbot-widget.blade.php
@@ -1,4 +1,4 @@
-<div x-data="chatbot()" class="fixed bottom-16 right-4 sm:right-8 z-[9000]">
+<div x-data="chatbot()" class="fixed bottom-6 right-6 z-[9000]">
     <button @click="open = !open" class="bg-[#D32F2F] hover:bg-red-700 text-white p-4 rounded-full shadow-2xl transition-transform hover:scale-105 flex items-center justify-center">
         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"></path></svg>
     </button>
